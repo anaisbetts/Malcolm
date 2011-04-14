9f8a41467a41e0aeb3659569752737a65ce5a3a3
Paul Betts <paul@paulbetts.org>
2011-04-14 13:35:48 -0700

    Initial Import

---
+++ RescueTimeByForce.Tests/RescueTimeTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using RescueTimeByForce;
+
+namespace RescueTimeByForce.Tests
+{
+    public class TestFact
+    {
+        [Fact]
+        public void Scratch()
+        {
+            var fixture = DateTime.Parse("2011-04-12T00:05:00");
+            Assert.Equal(2011, fixture.Year);
+            Assert.Equal(4, fixture.Month);
+            Assert.Equal(0, fixture.Hour);
+            Assert.Equal(5, fixture.Minute);
+        }
+
+        [Fact]
+        public void SplitLinesInBufferTest()
+        {
+            string input = "Hello World\nTest\n\nOther Stuff\n\n";
+            string[] expected = new[] { "Hello World", "Test", "Other Stuff" };
+
+            var output = ActivityRecord.splitLinesInBuffer(input).ToArray();
+
+            Assert.Equal(expected.Length, output.Length);
+            expected.Zip(output, (e, a) => new { e, a }).Run(x => Assert.Equal(x.e, x.a));
+        }
+
+        [Fact]
+        public void FetchRescueTimeDataTest()
+        {
+            var fixture = ActivityRecord.FetchRescueTimeData("B633MZSjMrwOV7N8lueArTYcauGGkYMUEIVgPCGW").First();
+            Assert.True(fixture.Length > 4);
+        }
+
+        [Fact]
+        public void TryCalculatingAggregateTimes()
+        {
+            var fixture = ActivityRecord.FetchRescueTimeData("B633MZSjMrwOV7N8lueArTYcauGGkYMUEIVgPCGW").First();
+
+            var lookup = fixture.ToLookup(x => x.Time);
+            var debugCalc = EnumerableEx.Generate(fixture.Min(x => x.Time), x => x <= fixture.Max(y => y.Time), x => x.AddMinutes(5.0), x => x)
+                .Select(x => new { Key = x, Sum = lookup[x].Sum(y => y.ProductivityInSeconds), Len = lookup[x].Count()})
+                .Scan0(new { Time = 0.0, Date = DateTime.MinValue } , (acc, x) => {
+                    var toAdd = x.Sum;
+                    if (x.Len == 0) {
+                        toAdd = 60.0 * (acc.Time > 0 ? -1.0 : 1.0);
+                    }
+                    var secs = (acc.Time + toAdd).Clamp(-10 * 60.0, 5 * 60.0);
+                    if (secs <= -10 * 60.0 && x.Sum != 0.0) {
+                        secs += 5 * 60.0;
+                    }
+                    return new {Time = secs, Date = x.Key};
+                }).ToList();
+
+            /*
+            var debugCalc = fixture
+                .ToLookup(x => x.Time)
+                .Select(x => new {x.Key, Sum = x.Sum(y => y.ProductivityInSeconds)})
+                .Scan0(new { Time = 0.0, Date = DateTime.MinValue } , (acc, x) => {
+                    var secs = (acc.Time + x.Sum).Clamp(-10 * 60.0, 5 * 60.0);
+                    if (secs <= -10 * 60.0 && x.Sum != 0.0) {
+                        secs += 5 * 60.0;
+                    }
+                    return new {Time = secs, Date = x.Key};
+                }).ToList();
+             */
+
+            var series = EnumerableEx.Generate(fixture.Min(x => x.Time), x => x <= fixture.Max(y => y.Time), x => x.AddMinutes(5.0), x => x)
+                .Select(x => new { Key = x, Sum = lookup[x].Sum(y => y.ProductivityInSeconds), Len = lookup[x].Count()})
+                .Aggregate(new {List = new List<DateTime>(), Secs = 0.0}, (acc, x) => {
+                    var toAdd = x.Sum;
+                    if (x.Len == 0) {
+                        toAdd = 60.0 * (acc.Secs > 0 ? -1.0 : 1.0);
+                    }
+                    var secs = (acc.Secs + toAdd).Clamp(-10 * 60.0, 5 * 60.0);
+                    if (secs <= -10 * 60.0 && x.Sum != 0.0) {
+                        acc.List.Add(x.Key);
+                        secs += 5 * 60.0;
+                    }
+
+                    return new {acc.List, Secs = secs};
+                }).List;
+
+            Assert.True(false);
+        }
+    }
+}
---
+++ RescueTimeByForce/RescueTime.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using ReactiveUI;
+
+namespace RescueTimeByForce
+{
+    public class ActivityRecord
+    {
+        public DateTime Time { get; protected set; }
+        public TimeSpan Duration { get; protected set; }
+        public int ProductivityScore { get; protected set; }
+
+        public double ProductivityInSeconds {
+            get { return Duration.TotalSeconds * (ProductivityScore / 1.0); }
+        }
+
+        public ActivityRecord(DateTime time, TimeSpan duration, int productivityScore)
+        {
+            Time = time;
+            Duration = duration;
+            ProductivityScore = productivityScore;
+        }
+
+        public static ActivityRecord ParseRecord(string record)
+        {
+            string[] fields = record.Split(',');
+            DateTime dt;
+            int seconds;
+            int ps;
+
+            if (!DateTime.TryParse(fields[0], out dt)) {
+                return null;
+            }
+
+            if (!Int32.TryParse(fields[1], out seconds)) {
+                return null;
+            }
+
+            if (!Int32.TryParse(fields[5], out ps)) {
+                return null;
+            }
+
+            return new ActivityRecord(dt, TimeSpan.FromSeconds(seconds), ps);
+        }
+
+        public static IEnumerable<string> splitLinesInBuffer(string buffer)
+        {
+            int start = 0;
+            for(int i=0; i < buffer.Length; i++) {
+                if (buffer[i] != '\n') {
+                    continue;
+                }
+
+                int len = i - start;
+                if (len > 0) {
+                    yield return buffer.Substring(start, len);
+                }
+
+                start = i + 1;
+            }
+
+            if (buffer.Length - start > 1) {
+                yield return buffer.Substring(start);
+            }
+        }
+
+        public static ActivityRecord[] ParseRescueTimeCSV(Stream data)
+        {
+            var ms = new MemoryStream(); data.CopyTo(ms);
+            var csv = Encoding.UTF8.GetString(ms.GetBuffer());
+
+            return splitLinesInBuffer(csv)
+                .Skip(1)
+                .Select(ParseRecord)
+                .Where(x => x != null)
+                .ToArray();
+        }
+
+        public static IObservable<ActivityRecord[]> FetchRescueTimeData(string apiKey)
+        {
+            var now = DateTime.Now;
+            string url = String.Format("https://www.rescuetime.com/anapi/data?key={0}&format=csv&pv=interval&rb={1}-{2:00}-{3:00}&re={1}-{2:00}-{3:00}&rs=minute",
+                apiKey, now.Year, now.Month, now.Day);
+
+            var hwr = WebRequest.Create(url) as HttpWebRequest;
+            var fetchFunc = Observable.FromAsyncPattern<WebResponse>(hwr.BeginGetResponse, hwr.EndGetResponse);
+
+            return fetchFunc().Select(x => ParseRescueTimeCSV(x.GetResponseStream()));
+        }
+    }
+
+    public static class DoubleMixin
+    {
+        public static double Clamp(this double This, double? min = null, double? max = null)
+        {
+            min = min ?? Double.MinValue;
+            max = max ?? Double.MaxValue;
+
+            double ret = This > max.Value ? max.Value : This;
+            return (ret < min.Value ? min.Value : ret);
+        }
+    }
+}
